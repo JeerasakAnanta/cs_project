08b472f72f28d2450bfd88e5e69e518a402d78de
Game <jeerasakananta@gmail.com>
2024-12-06 12:22:23 +0700

    Revert "docs :  init  readme  ChatBot API"
    This reverts commit aa549b139364ae2e93485f236ed0c970944c8cdd.

--- chatbot_api/run_api.sh
+++
@@ -1,4 +0,0 @@
-# Start FastAPI server
-echo "--------------- Start ChatBots  API -----------------"
-uvicorn chatbot_api:app --host 0.0.0.0 --port 8003 --reload
-echo "-----------------------------------------------------"
