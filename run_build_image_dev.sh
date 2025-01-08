2f8451eaa92da8ee61b84a141e81b8f87503535f
Game <jeerasakananta@gmail.com>
2025-01-08 15:01:30 +0700

    chore: update run_build_image_dev.sh to enhance dependency copying and improve script documentation

--- run_build_image_dev.sh
+++ run_build_image_dev.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# author: jeersak
+# author: Jeerasak Ananta  
 # date: 27/11/2024
+# version: 0.1.0 
+
 
 start_time=$(date +%H:%M:%S)
 
@@ -20,14 +22,21 @@ echo "------------------------------------------------------------"
 docker-compose down
 
 # copy  dpendency pypoetry 
-
-
+# move pyproject.toml  
 cp -r ./pyproject.toml ./chatbot_api/pyproject.toml
+
 cp -r ./pyproject.toml ./chatbot_pdf_management_api/pyproject.toml
 
+#  file requirements.txt  
+cp -r ./requirements.txt ./chatbot_api/requirements.txt
+cp -r ./requirements.txt ./chatbot_pdf_management_api/requirements.txt
+
+    
 echo "------------------------------------------------------------"
 echo "                  Copy Dpendency Pypoetry Done              " 
 echo "------------------------------------------------------------"
+
+pip freeze > requirements.txt 
 
 # Build image 
 docker build -t chatbot_webui_cs_project:${CHATBOT_WEBUI} ./chatbot_web/.
