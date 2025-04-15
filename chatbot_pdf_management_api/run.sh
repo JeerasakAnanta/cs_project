4cba4e4eaf1f9e859f004ed6077133907f156ecc
Game <jeerasakananta@gmail.com>
2025-04-15 22:06:43 +0700

    feat: add run script for starting PDF Management API

--- chatbot_pdf_management_api/run.sh
+++
@@ -1,3 +0,0 @@
-echo "---------------- Start PDF  Managements API    ---------------------"
-uvicorn pdf_management_api:app --host 0.0.0.0 --port 8004 --reload
-echo "--------------------------------------------------------------------"
