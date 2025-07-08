c345d8df5dc0bdac462511c5e303fc950b3ce4b0
Game <jeerasakananta@gmail.com>
2025-07-08 10:03:04 +0700

    refactor: remove PDF loading logic and streamline vector database creation

--- chatbot_pdf_management_api/create_vecter_db.py
+++ chatbot_pdf_management_api/create_vecter_db.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import glob
 import os
 
 from dotenv import load_dotenv
-from langchain.document_loaders import PyMuPDFLoader
+
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from qdrant_client.http.models import Distance, VectorParams
+from qdrant_client import QdrantClient
+
 from tqdm import tqdm
 
 # setup environment variable
@@ -17,52 +19,27 @@ load_dotenv()
 
 # Load environment variables
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-# Initialize embeddings
 
+# Initialize embeddings
 embeddings = OpenAIEmbeddings(
     model="text-embedding-3-large",
 )
 
-# Load PDF using PyMuPDFLoader
-pdfs = glob.glob("./pdfs/*.pdf")
+print("===========================")
+print("Creating vector database...")
+print("===========================")
 
-print("==================================================")
-print(pdfs)
-print("pwd: ", os.getcwd())
-print("==================================================")
-print("start loading documents...")
+# Initialize Qdrant client
+client = QdrantClient(url=os.getenv("QDRANT_URL", "http://localhost:6333"))
 
-try:
-    # Load documents
-    for pdf_path in tqdm(pdfs):
-        loader = PyMuPDFLoader(pdf_path)
-        documents = loader.load()
-
-        # Split the text into chunks
-        text_splitter = CharacterTextSplitter(
-            separator="\n", chunk_size=1000, chunk_overlap=200
-        )
-        chunks = text_splitter.split_documents(documents)
-
-        # On-premise server deployment
-        # Ensure the correct URL and port are used.
-        # If running locally, use localhost or the actual IP address.
-        url = "http://0.0.0.0:6333"  # Adjust this if necessary
-
-        # Initialize QdrantVectorStore with the chunks
-        qdrant = QdrantVectorStore.from_documents(
-            chunks,
-            embeddings,
-            url=url,
-            prefer_grpc=False,  # Disable gRPC in case it's not enabled on the server
-            collection_name="my_documents",
-        )
-
-except Exception as e:
-    print(f"Error: {e}")
-
+client.create_collection(
+    collection_name="docs_collection",
+    vectors_config=VectorParams(
+        size=embeddings.embedding_dim, distance=Distance.COSINE
+    ),
+)
 # Wait for embeddings to finish
 print("==================================================")
-print("Embeddings complete :)")
+print("creating vecter db  :)")
 print("Your documents have been loaded into Qdrant!")
 print("==================================================")
