e5e88e5baa40ac68800485956557f9df72386e86
Game <jeerasakananta@gmail.com>
2025-07-16 18:20:24 +0700

    fix: correct command in run_api_dev.sh and update project description in pyproject.toml

--- chatbot_api/scripts/run_api_dev.sh
+++ chatbot_api/scripts/run_api_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start  Run FastAPI server for development 
 echo "--------------- Start ChatBots  API -----------------"
-uv run uvicorn app.main:app --host 0.0.0.0 --port 8001 --reload
+uvicorn app.main:app --host 0.0.0.0 --port 8001 --reload
 echo "-----------------------------------------------------"
