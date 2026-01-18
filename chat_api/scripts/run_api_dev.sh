ca05664c2544304cce6e575ba0eecbb4a09aeb61
game <jeerasakananta@gmail.com>
2026-01-18 15:44:04 +0700

    ci: update run_api_dev.sh to use 'uv run' for starting the ChatBots API

--- chat_api/scripts/run_api_dev.sh
+++ chat_api/scripts/run_api_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo "--------------- Start ChatBots API -----------------"
-uvicorn  app.main:app --host 0.0.0.0 --port 8001 --reload
+uv run uvicorn app.main:app --host 0.0.0.0 --port 8001 --reload
 echo "-----------------------------------------------------"
